8a709bf483b8b25d3ea79877a697137fb86751b2
jatin-yadav <jatinyadav19oct@gmail.com>
2024-11-18 23:48:13 +0530

    adding shadcn-ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Planto: Breath Natural</h1>
+      <Button>Click ME</Button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: {
-          1: "#1B2316",
-        },
-        content: "#ffffff",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: {
+  				'1': '#1B2316'
+  			},
+  			content: '#ffffff'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
