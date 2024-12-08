bea917ddb35f149dec2c922472aec060f912b04c
jatin-yadav <jatinyadav19oct@gmail.com>
2024-12-09 00:04:25 +0530

    desc updated

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -15,12 +15,15 @@ const AboutUs = () => {
             <div className={`${styles.padding} flex flex-col-reverse lg:flex-row`}>
                 <div className='w-full lg:w-1/2'>
 
-                    <h1 className='text-2xl font-semibold'>From our hands to your home</h1>
-                    <p>
-                        Every plant you receive from us has been grown with love on our farm in Talegaon, just outside Pune. We nurture them for years before carefully packing and shipping them to your doorstep.
+                    <h1 className='text-2xl font-semibold py-4 underline underline-offset-8 decoration-green-500 '>From our hands to your home</h1>
+                    <p className='pb-2'>
+                        Every plant you welcome into your home is more than just greenery; it’s a piece of our heart. Grown with dedication on our serene farm in Talegaon, just outside Pune, each plant starts its journey as a tiny sapling, nurtured with care and patience. For years, we provide them with the perfect balance of sunlight, water, and nutrients, fostering their growth in harmony with nature.
                     </p>
-                    <p>
-                        {`It's`} truly a labour of love and patience.
+                    <p className='pb-2'>
+                        Our commitment goes beyond growing plants; it’s about creating a connection between you and the beauty of nature. Once a plant is ready to flourish in its new home, we handle it with the utmost care, meticulously packing and shipping it to ensure it reaches you in pristine condition.
+                    </p>
+                    <p className='pb-2'>
+                        At the core of our work is a passion for sustainability, love for greenery, and a belief in the power of plants to transform spaces and lives. From our hands to your home, every plant is a labor of love, patience, and a deep respect for nature—because we believe that every home deserves a touch of green magic.
                     </p>
                 </div>
                 <div className=' w-full lg:w-1/2 flex justify-center items-center '>
